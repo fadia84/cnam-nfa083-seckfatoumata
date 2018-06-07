5f4df9b18d2775a1b77856971fe73d9b988e26fe
fadia84 <infoweb094@gmail.com>
2018-06-07 03:15:13 +0200

    premiere modif

--- index.php
+++ index.php
@@ -1,18 +1,28 @@
 <!-- fichier cnam/nfa017/2017-cf1/index.inc.php - 20180604-PBO -->
+<!DOCTYPE html>
+<html lang='fr'>
+<head >
+    <meta charset="utf-8">
+    <title> hébergeur web</title>
+    <meta name="description" content="hébergement web">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 
 <!-- ENTETE HTML =============================================================== --><?php
      include("entete.inc.php");
      entete("NFA083 CF1 : Présentation hébergeur",0); /* pour le title et le h1 */ ?>
-
+</head>
+<body>
 <!-- PRESENTATION Hébergeur ============================================================== -->
-  <section><h2>Informations sur mon hébergeur</h2>
+  <section>
+    <h2>Informations sur mon hébergeur</h2>
            <p><strong>Nom : </strong> Gandi</p>
            <p><strong>Logo : </strong> <img src="img/gandi.jpg" /></p>
            <p><strong>Siège social : </strong> 63, 65 Boulevard Massena 75013 Paris</p>
            <p><strong>Lien d'inscription : </strong>
            <a href="https://www.gandi.net/fr/simple-hosting" target="_blanck" >Inscription Gandi</a></p>
   </section>
-  <section><h2>Informations sur la page question.php</h2>
+  <section>
+    <h2>Informations sur la page question.php</h2>
            <p><strong>Temps de chargement total en ms( https://tools.pingdom.com/ ): </strong> 2 050 s</p>
            <p><strong>Temps lié hébergeur (ms) : </strong> 1 200 s</p>
            <p><strong>Temps lié image (ms) : </strong> 46 ms</p>
@@ -22,7 +32,8 @@
            (http://www.mon-ip.com/adresse-ip-site-internet.php) : </strong> 217.70.180.152</p>
            <p><strong>Nom du serveur d'hébergement de la page question.php : </strong> gpaas12.dc0.gandi.net</p>
   </section>
-  <section><h2>Complément d'information</h2>
+  <section>
+    <h2>Complément d'information</h2>
            <p><strong>Une question complémentaire ? </strong> 
               <a href="questions.php" id="question" class="actif">Poser une question</a>
            </p>
